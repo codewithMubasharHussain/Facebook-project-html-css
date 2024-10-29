192486755ff5d7a578aec287f3d542ecfb1a12ca
Mubashar Hussain <codewithmubasharhussain@gmail.com>
2024-10-29 08:32:03 +0500

    adding dark mode

--- script.js
+++ script.js
@@ -1,5 +1,10 @@
 var setting = document.querySelector(".user-setting");
+var darkbtn  = document.getElementById("dark-btn");
 function settingmenutoggle(){
 
     setting.classList.toggle("user-setting-height")
+}
+darkbtn.onclick = function(){
+    darkbtn.classList.toggle("dark-btn-on");
+    document.body.classList.toggle("dark-theme");
 }
